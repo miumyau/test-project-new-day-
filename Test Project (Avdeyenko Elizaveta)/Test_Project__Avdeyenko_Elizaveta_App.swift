897b18526c984b41e4def9d83aa64822a783a193
root <elliss.avd@gmail.com>
2024-07-30 14:20:09 +0300

    Initial Commit

---
+++ Test Project (Avdeyenko Elizaveta)/Test_Project__Avdeyenko_Elizaveta_App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Project__Avdeyenko_Elizaveta_App.swift
+//  Test Project (Avdeyenko Elizaveta)
+//
+//  Created by Елизавета on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_Project__Avdeyenko_Elizaveta_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
